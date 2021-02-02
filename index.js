a3fc06a9a9ddcb32e7a7714231800e2e293596c7
mcapurri <m.capurri@gmail.com>
2021-02-02 15:02:43 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        (this.items = []), (this.length = this.items.length);
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (!this.items[pos]) {
+            throw new Error('OutOfBounds');
+        } else {
+            return this.items[pos];
+        }
+    }
 
-  max() {}
+    max() {
+        if (!this.items) {
+            throw new Error('EmptySortedList');
+        } else {
+            return Math.max(this.items);
+        }
+    }
 
-  min() {}
+    min() {
+        if (!this.items) {
+            throw new Error('EmptySortedList');
+        } else {
+            return Math.min(this.items);
+        }
+    }
 
-  sum() {}
+    sum() {
+        if (!this.items) {
+            return 0;
+        } else {
+            return this.items.reduce((acc, num) => acc + num);
+        }
+    }
 
-  avg() {}
+    avg() {
+        if (!this.items) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items.reduce((acc, num) => acc + num) / this.length;
+        }
+    }
 }
 
 module.exports = SortedList;
